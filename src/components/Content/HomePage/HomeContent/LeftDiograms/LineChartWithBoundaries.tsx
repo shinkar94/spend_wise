06fa72db2af6eea0e3be8127463c90ff12fa99e8
shinkar94 <romka94shinkarenko@gmail.com>
2023-08-19 11:05:21 +0300

    fix: delete left diagrams

--- src/components/Content/HomePage/HomeContent/LeftDiograms/LineChartWithBoundaries.tsx
+++ src/components/Content/HomePage/HomeContent/LeftDiograms/LineChartWithBoundaries.tsx
@@ -1,7 +1,6 @@
-import React, {useRef, useEffect} from "react";
-import {Chart, ChartOptions, LineControllerDatasetOptions, registerables} from 'chart.js';
+import React, {useRef} from "react";
+import {Chart, registerables} from 'chart.js';
 import {useAppSelector} from "@/hok/hoks";
-
 
 // Регистрируем необходимые модули
 Chart.register(...registerables);
@@ -13,90 +12,9 @@ type DataType = {
 }
 
 export const LineChartWithBoundaries = () => {
-    const chartRef = useRef<HTMLCanvasElement>(null); // Используем useRef с типом элемента
-    const state = useAppSelector(state => state.allState)
-    const stateOutcome = state.filter(el => el.type === 'outcome')
+    const chartRef = useRef<HTMLCanvasElement>(null);
+    const state = useAppSelector(state => state.allState);
+    const stateOutcome = state.filter(el => el.type === 'outcome');
 
-
-    const arr: Array<number[]> = Array.from({length: 12}, () => []);
-    stateOutcome.forEach(({date, value}) => {
-        const month = Number(date.split("-")[1]) - 1;
-        // console.log(month)
-        arr[month].push(value);
-    });
-    const arr2 = arr.map(subarr => subarr.reduce((acc, el) => acc + el, 0));
-
-
-    const data: DataType = {
-        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        values: arr2
-    }
-
-    useEffect(() => {
-        // Проверяем, что элемент canvas существует
-        if (chartRef && chartRef.current) {
-
-
-            const gradient = (context: CanvasRenderingContext2D | null) => {
-                if (!context) return null;
-                const gradient = context.createLinearGradient(0, 0, 0, 450);
-                gradient.addColorStop(0, 'rgba(125,73,112,0.6)');
-                gradient.addColorStop(1, 'rgba(61,56,146, 0.3)');
-                return gradient;
-            }
-
-            const context: CanvasRenderingContext2D | null | undefined = chartRef.current?.getContext('2d');
-            // if (!context) return null; // если значение context равно undefined, вернуть null
-
-            // @ts-ignore
-            const config: ChartOptions<"line", LineControllerDatasetOptions[], string> = {
-                type: "line",
-                data: {
-                    labels: data.labels,
-                    datasets: [
-                        {
-                            label: "outcome",
-                            data: data.values,
-                            borderColor: "blue",
-                            fill: "start",
-                            borderWidth: 0.2,
-                            pointBackgroundColor: "rgb(125,73,112)",
-                            backgroundColor: gradient(context),
-                            cubicInterpolationMode: 'monotone',
-                        }
-                    ],
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        xAxes: [
-                            {
-                                type: "time",
-                                time: {
-                                    unit: "day",
-                                },
-                                ticks: {
-                                    autoSkip: true,
-                                    maxTicksLimit: 10,
-                                },
-                            }
-                        ]
-                    },
-                },
-            }
-
-
-            // Получаем контекст для рисования на холсте
-            const chartCanvas = chartRef.current.getContext("2d");
-            if (!chartCanvas) return;
-
-            // Создаем новый график
-            const chart = new Chart(chartCanvas, config);
-
-            return () => chart.destroy(); // Уничтожаем график при размонтировании компонента
-        }
-    }, [data]);
-
-    return <canvas ref={chartRef}/>; // Используем типизированный ref для элемента canvas
+    return <canvas ref={chartRef} />;
 };

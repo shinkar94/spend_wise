aac1e605907147f471409c047803c7fd126795f8
shinkar94 <romka94shinkarenko@gmail.com>
2023-08-19 11:10:08 +0300

    fix: fix error for deploy

--- src/components/Content/HomePage/HomeContent/LeftDiograms/LineChartWithBoundaries.tsx
+++ src/components/Content/HomePage/HomeContent/LeftDiograms/LineChartWithBoundaries.tsx
@@ -1,20 +1,8 @@
-import React, {useRef} from "react";
-import {Chart, registerables} from 'chart.js';
-import {useAppSelector} from "@/hok/hoks";
-
-// Регистрируем необходимые модули
-Chart.register(...registerables);
-
-// Определяем типы данных для графика
-type DataType = {
-    labels: string[],
-    values: number[]
-}
-
+import React from "react";
 export const LineChartWithBoundaries = () => {
-    const chartRef = useRef<HTMLCanvasElement>(null);
-    const state = useAppSelector(state => state.allState);
-    const stateOutcome = state.filter(el => el.type === 'outcome');
-
-    return <canvas ref={chartRef} />;
+    return (
+        <>
+            left diagram
+        </>
+    )
 };

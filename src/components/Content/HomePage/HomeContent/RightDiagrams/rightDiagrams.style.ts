ca5655a8c1a89d516210354082a4d827dec35d8c
Alik_Avizha <aleksandravizha@icloud.com>
2023-08-25 12:22:07 +0300

    fix drawing of the right graph

--- src/components/Content/HomePage/HomeContent/RightDiagrams/rightDiagrams.style.ts
+++ src/components/Content/HomePage/HomeContent/RightDiagrams/rightDiagrams.style.ts
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 const DiagramsWrapper = styled.div`
   padding: 20px;
-
+  height: 100%;
 `
 const Diagram = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
-  height: 300px;
+  max-height: 300px;
 `
 
 export const S = {DiagramsWrapper,Diagram}

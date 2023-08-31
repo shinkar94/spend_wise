e5eab1ea8c7b060bbfc65c7b15c7f247c2a69ba0
shinkar94 <romka94shinkarenko@gmail.com>
2023-08-31 22:01:06 +0300

    fix: Cookie lifetime has been increased to 30 days

--- src/app/service/set-cooke/setCooke.ts
+++ src/app/service/set-cooke/setCooke.ts
@@ -4,7 +4,7 @@ import {cookies} from "next/headers";
 export const SetCooke = (accessToken: string | undefined) => {
     const cookieOptions = {
         httpOnly: true, // Кука не будет доступна через JavaScript
-        maxAge: 30 * 60 * 1000, // 30 минут (в миллисекундах)
+        maxAge: 30 * 24 * 60 * 60 * 1000 //30 дней
     };
     accessToken && cookies().set('accessToken', accessToken, cookieOptions)
 }
